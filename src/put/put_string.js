1ae9120a2eb22d211ec6148c2eaaf417080e568c
Tilican <titben047@hotmail.fr>
2019-05-09 22:12:02 +0200

    put_string != put_var(string)

--- src/put/put_string.js
+++ src/put/put_string.js
@@ -1,17 +1,9 @@
 function putString (value) {
   let len = Buffer.byteLength(value)
-  let pad = len % 4 === 0 ? 0 : 4 - len % 4
-  let newBuffer = Buffer.allocUnsafe(4 + len + pad)
+  let newBuffer = Buffer.allocUnsafe(4 + len)
 
   newBuffer.writeUInt32LE(len, 0)
   newBuffer.write(value, 4)
-  if (pad !== 0) {
-    let pos = 4 + len
-
-    for (let i = 0; i < pad; i++) {
-      newBuffer.write('\0', i + pos)
-    }
-  }
   return newBuffer
 }
 
