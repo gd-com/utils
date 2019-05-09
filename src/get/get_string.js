b89e823a6a84921cef419a65f909201c148ec23b
Tilican <titben047@hotmail.fr>
2019-05-09 22:12:02 +0200

    put_string != put_var(string)

--- src/get/get_string.js
+++ src/get/get_string.js
@@ -1,10 +1,9 @@
 function getString (buffer, offset = 0) {
   const len = buffer.readUInt32LE(offset)
-  const pad = len % 4 === 0 ? 0 : 4 - (len % 4)
 
   return {
     value: buffer.toString('utf8', offset + 4, offset + 4 + len).replace('\u0000', ''),
-    length: 4 + len + pad
+    length: 4 + len
   }
 }
 
--- src/put/put_var/string.js
+++ src/put/put_var/string.js
@@ -1,6 +1,22 @@
 const { STRING } = require('../../constants')
 const putU32 = require('../put_u32')
-const putString = require('../put_string')
+
+function putString (value) {
+  let len = Buffer.byteLength(value)
+  let pad = len % 4 === 0 ? 0 : 4 - len % 4
+  let newBuffer = Buffer.allocUnsafe(4 + len + pad)
+
+  newBuffer.writeUInt32LE(len, 0)
+  newBuffer.write(value, 4)
+  if (pad !== 0) {
+    let pos = 4 + len
+
+    for (let i = 0; i < pad; i++) {
+      newBuffer.write('\0', i + pos)
+    }
+  }
+  return newBuffer
+}
 
 /**
  * Encode string
