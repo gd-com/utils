75cf00808ddc02a6825c37b92c9a5762ff0f0b1e
Tilican <evandikt@gmail.com>
2021-11-29 10:08:16 +0100

    Update

--- src/get/get_var/rawArray.ts
+++ src/get/get_var/rawArray.ts
@@ -6,11 +6,11 @@ import {IGetReturn} from "../../types";
  * @param buf {Buffer}
  * @returns {{value: Array, length: Number}}
  */
-export function getVarRawArray (genericDecoder, buf: Buffer): IGetReturn<Array<number>> {
+export function getVarRawArray (genericDecoder, buf: Buffer): IGetReturn<Buffer> {
   const bufLength = buf.readUInt32LE(0)
 
   return {
-    value: buf.slice(4, bufLength + 4).toJSON().data,
+    value: buf.slice(4, bufLength + 4),
     length: bufLength + 4 + 4 // type +  length
   }
 }
